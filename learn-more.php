b41a3d509c2668668151448f5ca2cab18e0ac789
mbostwick23 <33204834+mbostwick23@users.noreply.github.com>
2017-10-30 14:47:31 -0400

    Update learn-more.php

--- learn-more.php
+++ learn-more.php
@@ -1,1 +1,26 @@
-
+<?php
+include ('header.php');
+include ('top.php');
+include ('nav.php');
+?>
+<!--start of Origins article-->
+<article id="origin">
+  <h1>The Origins of Language</h1>
+  <p>put content here</p>
+  <figure class="img-right"> <!---add more classes as needed-->
+    <img class="" alt="" src="">
+    <figcaption>put caption here</figcaption>
+  </figure>
+</article>
+<!--start of Importance article, we may not need 2 articles, depends on CSS we want-->
+<article id="importance"> 
+  <h1>Why is Language Important?</h1>
+  <p>put content here</p>
+  <figure class="img-right"> <!---add more classes as needed-->
+    <img class="" alt="" src="">
+    <figcaption>put caption here</figcaption>
+  </figure>
+</article>
+<?php
+include ('footer.php');
+?>
