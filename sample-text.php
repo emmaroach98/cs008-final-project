38e257baf0de1750d0e59b74a98d9911b625dd8b
mbostwick23 <33204834+mbostwick23@users.noreply.github.com>
2017-10-30 14:50:16 -0400

    Update sample-text.php

--- sample-text.php
+++ sample-text.php
@@ -1,1 +1,28 @@
-
+<?php
+include ('header.php');
+include ('top.php');
+include ('nav.php');
+?>
+<article id="text1"> 
+  <h1>put title here</h1>
+  <p>put text here</p>
+  <!--add paragraphs as needed depending on content-->
+</article>
+<article id="text2"> 
+  <h1>put title here</h1>
+  <p>put text here</p>
+  <!--add paragraphs as needed depending on content-->
+</article>
+<article id="text3"> 
+  <h1>put title here</h1>
+  <p>put text here</p>
+  <!--add paragraphs as needed depending on content-->
+</article>
+<article id="text4"> 
+  <h1>put title here</h1>
+  <p>put text here</p>
+  <!--add paragraphs as needed depending on content-->
+</article>
+<?php
+include ('footer.php');
+?>
