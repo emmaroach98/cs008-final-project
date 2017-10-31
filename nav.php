d1d0245ad3e9f441eaf6b39d4055294b67731805
mbostwick23 <33204834+mbostwick23@users.noreply.github.com>
2017-10-31 19:46:03 -0400

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,57 @@
+<!-- ######################    Start of Nav   ########################## -->
+<nav>
+    <ol>
+        <?php       
+        print '<li class="';
+        if ($path_parts[ 'filename' ] == "index") {
+            print ' activePage ';
+        }
+        print '">';
+        print'<a href="index.php">Home</a>'; <!--may have to chage the href slightly depending on which folders the file is in-->
+        print'</li>';
+        
+        print '<li class="';
+        if ($path_parts[ 'filename' ] == "learn-more") {
+            print ' activePage ';
+        }
+        print '">';
+        print'<a href="learn-more.php">Learn More</a>';
+        print'</li>';
+        
+        print '<li class="';
+        if ($path_parts[ 'filename' ] == "about-authors") {
+            print ' activePage ';
+        }
+        
+        print '">';
+        print'<a href="about-authors.php">About the Authors</a>';
+        print'</li>';
+        
+        print '<li class="';
+        if ($path_parts[ 'filename' ] == "sample-text") {
+            print ' activePage ';
+        }
+        
+        print '">';
+        print'<a href="sample-text.php">Sample Texts</a>';
+        print'</li>';
+        
+        print '<li class="';
+        if ($path_parts[ 'filename' ] == "form") {
+            print ' activePage ';
+        }
+        print '">';
+        print'<a href="form.php">Form</a>';
+        print'</li>';
+        
+        print '<li class="';
+        if ($path_parts[ 'filename' ] == "blog") {
+            print ' activePage ';
+        }
+        print '">';
+        print'<a href="blog.php">Blog</a>';
+        print'</li>';
+        ?>
+    </ol>
+</nav>
+<!-- ######################    End of Nav   ########################## -->
