5a14fb4c8e1f5934074ab01da15f82c2a6f6a20f
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-02 19:02:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-
+<?php
+include 'top.php';
+?>
+<article>
+    <h1>The Art of Translation</h1>
+    
+    <figure class="index-right-small">
+        <img src="" alt="">
+        <figcaption></figcaption>
+    </figure>
+    
+    <p>put text here</p>
+</article>
+<?php
+include ('footer.php');
+?>  
+</body>
+</html>
