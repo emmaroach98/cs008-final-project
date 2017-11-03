3a1d85067ca2226d5d63fb3d5c5bea08d31ce898
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-02 21:05:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,37 @@ include 'top.php';
         <figcaption></figcaption>
     </figure>
     
-    <p>put text here</p>
+    <p>In a world where countries have enough trouble communicating with each 
+        other, dealing with language barriers only makes it that much harder to 
+        keep peace in the world. This is where translation comes into play: 
+        translation allows people who speak different languages to communicate 
+        easily. The art of translation plays an important role in how we view and 
+        communicate with people in other countries.</p>
+    <p>Verbal translation can happen every day within the mind of a bilingual 
+        person. Even in our own language, sometimes we have to translate into 
+        words we understand. Written translation between two different languages, 
+        particularly of books and literary works, allows us to bring a new aspect 
+        of language into our lives - we are able to read and understand something 
+        that was originally written in a language we don’t understand. Translators 
+        play a very important role in the connection between different countries, 
+        whether they translate verbally or written works. Both are equally 
+        important in the connection between cultures in the world.</p>
+    <p>Translation may seem like an easy task, something that Google Translate 
+        can do for you instantly. However, true translation, translation that 
+        includes emotion and idiomatic meaning can only be done by a human 
+        translator. No matter how good machine translators get, they will never 
+        be able to compare to the language a translator can communicate. The art 
+        of translation is tricky, because one needs to have a very strong 
+        understanding and ability of multiple languages. The ability to speak more 
+        than one language is an experience like no other, an experience that every 
+        person should be able to have.</p> 
+    <p>Thus, we here at NAME OF COMPANY are working towards making multi-lingual 
+        texts available to as many people as possible. You can view some examples 
+        on our <a href="sample-text.php">Sample Texts</a> page, and even put in a request for a text to be 
+        translated on our <a href="form.php">Request a Translation</a> page. We want to bring the literature 
+        of other cultures to you as easily as possible. Please visit our blog page 
+        to see how other users have benefitted from our services, and request a 
+        translation to begin your multi-lingual pursuit today.</p>
 </article>
 <?php
 include ('footer.php');
