5dcac5348f4e2357ff415fa26574f2989b460d07
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-02 20:26:30 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,5 @@
-
+<!-- ######################     Start of header   ############################## -->
+<header class="headerTitle">
+    <h1>name of company</h1>
+</header>
+<!-- ######################     End of header   ############################## -->
