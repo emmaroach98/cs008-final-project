f24bec702cabd02349777b1475fbc2efd4377b59
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-04 20:27:02 -0400

    Update blog.php

--- blog.php
+++ blog.php
@@ -3,6 +3,10 @@ include 'top.php';
 ?>
 <article>        
     <h1>Blog</h1>
+    <p>put text here</p>
+    <p>put text here</p>
+    <p>put text here</p>
+    <!-- add more for more posts -->
 </article>
 <?php
 include ('footer.php');
