e1874b89e59cadd47bb3b54e4776c2a45adfdbc1
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-04 20:43:53 -0400

    Update blog.php

--- blog.php
+++ blog.php
@@ -3,7 +3,11 @@ include 'top.php';
 ?>
 <article>        
     <h1>Blog</h1>
-    <p>put text here</p>
+    <p>Welcome to our blog! Here, we just share some feedback left from different 
+        users of our website. Feel free to leave us comments in our request form, 
+        and you might be featured on our blog page. We encourage everyone to leave 
+        comments in their native language (we will translate) so that we can 
+        continue the connection between different languages. Thank you!</p>
     <p>put text here</p>
     <p>put text here</p>
     <!-- add more for more posts -->
