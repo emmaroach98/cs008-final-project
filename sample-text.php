c39825510fb63d49769fe09c70ca9ecde641724d
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-04 21:04:50 -0400

    Update sample-text.php

--- sample-text.php
+++ sample-text.php
@@ -29,8 +29,23 @@
    <!--add paragraphs as needed depending on content-->
  </article>
  <article id="text3"> 
-   <h1>put title here</h1>
-   <p>put text here</p>
+   <h1>Hänsel und Gretel - The Brothers Grimm, 1812</h1>
+   <p>Vor einem großen Wald wohnte ein armer Holzhacker mit seiner Frau und seinen
+   zwei Kingers; das Bübchen hieß Hänsel und das Mädchen Gretel. Er hatte wenig 
+   zu beißen und zu brechen, und einmal, als große Teuerung ins Land kam, konnte
+   er auch das täglich Brot nicht mehr schaffen. Wie er sich nun abends im Bette
+   Gedanken machte und sich vor Sorgen herumwälzte, seufzte er und sprach zu seiner
+   Frau: "Was soll aus uns werden? Wie können wir unsere armen Kinder ernähren,
+   da wir für uns selbst nichts mehr haben?" "Wießt du was, Mann", antwortete die 
+   Frau, "wir wollen morgen in aller Frühe die Kinder hinaus in den Wald führen, 
+   wo er am dicksten ist: da machen wir ihnen ein Feuer an und geben jedem noch 
+   ein Stückchen Brot, dann gehen wir am unsere Arbeit und lassen sie allein. Sie
+   finden den Weg nicht wieder nach Haus, und wir sind sie los." "Nein, Frau", 
+   sagte der Mann, "das tue ich nicht; wie sollt' ich's übers Herz bringen, meine
+   Kinder im Walde allein zu lassenm die wilden Tiere würden bald kommen und sie 
+   zerreißen." "O du Narr", sagte sie, "dann müssen wir alle viere Hungers sterben, 
+   du kannst nur die Bretter für die Särge hobelen", und ließ ihm keine Ruhe, bis
+   er einwilligte. "Aber die armen Kinder dauern mich doch", sagte der Mann.</p>
    <!--add paragraphs as needed depending on content-->
  </article>
  <article id="text4"> 
