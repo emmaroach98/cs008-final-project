24abf7eb88bab92922e7ad3812762a0027457f1f
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-04 20:25:19 -0400

    Update sample-text.php

--- sample-text.php
+++ sample-text.php
@@ -3,7 +3,24 @@
  ?>
  <article id="text1"> 
    <h1>put title here</h1>
-   <p>put text here</p>
+   <p>Es war einmal mitten im Winter, und die Schneeflocken fielen wie Federn vom 
+    Himmel herab, da saß eine Köngin an einem Fenster, das einen Rahmen von 
+    scharzem Ebenholz hatte, und nähte. Und wie soe sp nähte und nach dem Schnee 
+    aufblickte, stach sie sich mit der Nadel in den Finger, und es fielen drei 
+    Tropfen Blut in den Schnee. Und weil das Rote im weißen Schnee so schön aussah,
+    dachte sie bei sich: "Hätt ich ein Kind so weiß wie Schnee, so rot wie Blut 
+    und so schwarz wie das Holz an dem Rahmen." Bald darauf bekam sie ein 
+    Töchterlein, das war so weiß wie Schnee, so rot wie Blut und so schwarzhaaring 
+    wie Ebenholz, und ward darum das Schneewittchen (Schneeweißchen) genannt. 
+    Und wie das Kind geboren war, starb die Königin.</p>
+   <p>Über ein Jahr nahm sich der König eine andere Gemahlin. Es war eine schöne 
+    Frau, aber sie war stolz und übermütig und konnte nicht leiden, daß sie an 
+    Schönheit von jemand sollte übertroffen werden. Sie hatte einen wunderbaren 
+    Spiegel, wenn sie vor den trat und sich darin beschaute, sprach sie:
+    <span class="indentQuote">"Spieglein, Spieglein an der Wand,<br>wer ist die 
+    schönste im ganzen Land?</span><br>So antwortete der Spiegel: 
+    <span class="indentQuote">"Frau Königin, Ihr seid sie schönste im Land"</span><br>
+    Da war sie zufrienden, denn sie wußte, daß der Spiegel die Wahrheit sagte.</p>
    <!--add paragraphs as needed depending on content-->
  </article>
  <article id="text2"> 
