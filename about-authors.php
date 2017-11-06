655df63185825f739072bb346f01f893041b4b3e
Slamuel-Nard <32207628+Slamuel-Nard@users.noreply.github.com>
2017-11-05 22:10:37 -0500

    Update about-authors.php

--- about-authors.php
+++ about-authors.php
@@ -1,1 +1,55 @@
+<?php include ('top.php');
+?>
+  
+    <article class='authorBio'>
+        <h1 class="authorBioH1">Author Languages and Biography</h1>
+<?php
 
+        $myFolder = 'finalproject/';
+
+        $myFileName = 'aboutauthors';
+
+        $fileExt = '.csv';
+
+        $filename = $myFolder . $myFileName . $fileExt;
+
+       
+
+        $file=fopen($filename, "r");
+
+
+            // read the header row, copy the line for each header row
+            // you have.
+            $headers[] = fgetcsv($file);
+    
+            while (!feof($file)) {
+            $aboutAuthors[] = fgetcsv($file);
+        }
+        fclose($file);
+    
+        print '<table class="aboutAuthorTable">';
+        print '<thead class="aboutAuthorHeaders">';
+        foreach ($headers as $header){
+            print '<tr class="tableAuthorHeadingRow">';
+            print '<th>' .$header[0].'</th>';
+            print '<th>' .$header[1].'</th>';       
+            print '<th>' .$header[2].'</th>';
+            print '</tr>';
+        }
+        print '</thead>';
+    foreach ($aboutAuthors as $authorData) {
+        print '<tr>';
+    foreach ($authorData as $authorCells) {
+        print '<td>';
+        print $authorCells;
+        print '</td>;
+    } 
+        print '</tr>';
+    }
+    print '</table'>;
+    
+    include ('footer.php')
+    ?>
+    
+</article>
+  
