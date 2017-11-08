8a50fa7fd947d8bbdfa5f6b868fd545676bf9914
mbostwick23 <33204834+mbostwick23@users.noreply.github.com>
2017-11-07 19:34:40 -0500

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,15 +1,15 @@
 <?php
 include 'top.php';
-?>
-<article>        
+?>        
     <h1>Blog</h1>
     <p>Welcome to our blog! Here, we just share some feedback left from different 
         users of our website. Feel free to leave us comments in our request form, 
         and you might be featured on our blog page. We encourage everyone to leave 
         comments in their native language (we will translate) so that we can 
         continue the connection between different languages. Thank you!</p>
-    <p class="blogPost">put text here</p>
-    <p class="blogPost">put text here</p>
+<article id="blogPosts">    
+    <p class="post">put text here</p>
+    <p class="post">put text here</p>
     <!-- add more for more posts -->
 </article>
 <?php
