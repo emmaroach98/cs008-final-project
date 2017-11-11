e290a920c68ac2ccc276a0286911114323e4902c
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-11 13:54:31 -0500

    Update sample-text.php

--- sample-text.php
+++ sample-text.php
@@ -4,23 +4,21 @@
  <article id="text1"> 
    <h1>Sneewittchen - The Brothers Grimm, 1854</h1>
    <p>Es war einmal mitten im Winter, und die Schneeflocken fielen wie Federn vom 
-    Himmel herab, da saß eine Köngin an einem Fenster, das einen Rahmen von 
-    scharzem Ebenholz hatte, und nähte. Und wie soe sp nähte und nach dem Schnee 
-    aufblickte, stach sie sich mit der Nadel in den Finger, und es fielen drei 
-    Tropfen Blut in den Schnee. Und weil das Rote im weißen Schnee so schön aussah,
-    dachte sie bei sich: "Hätt ich ein Kind so weiß wie Schnee, so rot wie Blut 
-    und so schwarz wie das Holz an dem Rahmen." Bald darauf bekam sie ein 
-    Töchterlein, das war so weiß wie Schnee, so rot wie Blut und so schwarzhaaring 
-    wie Ebenholz, und ward darum das Sneewittchen (Schneeweißchen) genannt. 
-    Und wie das Kind geboren war, starb die Königin.</p>
-   <p>Über ein Jahr nahm sich der König eine andere Gemahlin. Es war eine schöne 
-    Frau, aber sie war stolz und übermütig und konnte nicht leiden, daß sie an 
-    Schönheit von jemand sollte übertroffen werden. Sie hatte einen wunderbaren 
-    Spiegel, wenn sie vor den trat und sich darin beschaute, sprach sie:
-    <span class="indentQuote">"Spieglein, Spieglein an der Wand,<br>wer ist die 
-    schönste im ganzen Land?</span><br>So antwortete der Spiegel: 
-    <span class="indentQuote">"Frau Königin, Ihr seid sie schönste im Land"</span><br>
-    Da war sie zufrienden, denn sie wußte, daß der Spiegel die Wahrheit sagte.</p>
+    Himmel herab, da saß eine Königin an einem Fenster, das einen Rahmen von schwarzem 
+    Ebenholz hatte, und nähte. Und wie sie so nähte und nach dem Schnee aufblickte, 
+    stach sie sich mit der Nadel in den Finger, und es fielen drei Tropfen Blut in den 
+    Schnee. Und weil das Rote im weißen Schnee so schön aussah, dachte sie bei sich: 
+    "Hätt ich ein Kind so weiß wie Schnee, so rot wie Blut und so schwarz wie das Holz 
+    an dem Rahmen." Bald darauf bekam sie ein Töchterlein, das war so weiß wie Schnee, 
+    so rot wie Blut und so schwarzhaarig wie Ebenholz, und ward darum das Sneewittchen 
+    (Schneeweißchen) genannt. Und wie das Kind geboren war, starb die Königin.</p>
+   <p>Über ein Jahr nahm sich der König eine andere Gemahlin. Es war eine schöne Frau, 
+    aber sie war stolz und übermütig und konnte nicht leiden, daß sie an Schönheit von 
+    jemand sollte übertroffen werden. Sie hatte einen wunderbaren Spiegel, wenn sie vor 
+    den trat und sich darin beschaute, sprach sie: </br>"Spieglein, Spieglein an der Wand,</br>
+    wer ist die schönste im ganzen Land?
+   <p>So antwortete der Spiegel: "Frau Königin, Ihr seid die schönste im Land."</p>
+   <p>Da war sie zufrieden, denn sie wußte, daß der Spiegel die Wahrheit sagte.</p>
    <!--add paragraphs as needed depending on content-->
  </article>
  <article id="text2"> 
@@ -30,22 +28,22 @@
  </article>
  <article id="text3"> 
    <h1>Hänsel und Gretel - The Brothers Grimm, 1812</h1>
-   <p>Vor einem großen Wald wohnte ein armer Holzhacker mit seiner Frau und seinen
-   zwei Kingers; das Bübchen hieß Hänsel und das Mädchen Gretel. Er hatte wenig 
-   zu beißen und zu brechen, und einmal, als große Teuerung ins Land kam, konnte
-   er auch das täglich Brot nicht mehr schaffen. Wie er sich nun abends im Bette
-   Gedanken machte und sich vor Sorgen herumwälzte, seufzte er und sprach zu seiner
-   Frau: "Was soll aus uns werden? Wie können wir unsere armen Kinder ernähren,
-   da wir für uns selbst nichts mehr haben?" "Wießt du was, Mann", antwortete die 
-   Frau, "wir wollen morgen in aller Frühe die Kinder hinaus in den Wald führen, 
-   wo er am dicksten ist: da machen wir ihnen ein Feuer an und geben jedem noch 
-   ein Stückchen Brot, dann gehen wir am unsere Arbeit und lassen sie allein. Sie
-   finden den Weg nicht wieder nach Haus, und wir sind sie los." "Nein, Frau", 
-   sagte der Mann, "das tue ich nicht; wie sollt' ich's übers Herz bringen, meine
-   Kinder im Walde allein zu lassenm die wilden Tiere würden bald kommen und sie 
-   zerreißen." "O du Narr", sagte sie, "dann müssen wir alle viere Hungers sterben, 
-   du kannst nur die Bretter für die Särge hobelen", und ließ ihm keine Ruhe, bis
-   er einwilligte. "Aber die armen Kinder dauern mich doch", sagte der Mann.</p>
+   <p>Vor einem großen Wald wohnte ein armer Holzhacker mit seiner Frau und seinen 
+    zwei Kindern; das Bübchen hieß Hänsel und das Mädchen Gretel. Er hatte wenig zu 
+    beißen und zu brechen, und einmal, als große Teuerung ins Land kam, konnte er 
+    auch das täglich Brot nicht mehr schaffen. Wie er sich nun abends im Bette Gedanken 
+    machte und sich vor Sorgen herumwälzte, seufzte er und sprach zu seiner Frau: 
+    "Was soll aus uns werden? Wie können wir unsere armen Kinder ernähren, da wir 
+    für uns selbst nichts mehr haben?" "Weißt du was, Mann", antwortete die Frau, 
+    "wir wollen morgen in aller Frühe die Kinder hinaus in den Wald führen, wo er 
+    am dicksten ist: da machen wir ihnen ein Feuer an und geben jedem noch ein Stückchen 
+    Brot, dann gehen wir an unsere Arbeit und lassen sie allein. Sie finden den Weg 
+    nicht wieder nach Haus, und wir sind sie los." "Nein, Frau", sagte der Mann, "das 
+    tue ich nicht; wie sollt' ich's übers Herz bringen, meine Kinder im Walde allein 
+    zu lassen die wilden Tiere würden bald kommen und sie zerreißen." "O du Narr", 
+    sagte sie, "dann müssen wir alle viere Hungers sterben, du kannst nur die Bretter 
+    für die Särge hobeln", und ließ ihm keine Ruhe, bis er einwilligte. "Aber die armen 
+    Kinder dauern mich doch", sagte der Mann.</p>
    <!--add paragraphs as needed depending on content-->
  </article>
  <article id="text4"> 
