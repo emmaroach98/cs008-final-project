63791750690626ae9a63d5a0460aba8bcee326db
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-12 15:50:12 -0500

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,15 +1,35 @@
 <?php
 include 'top.php';
-?>        
+?>
+<article>        
     <h1>Blog</h1>
     <p>Welcome to our blog! Here, we just share some feedback left from different 
         users of our website. Feel free to leave us comments in our request form, 
         and you might be featured on our blog page. We encourage everyone to leave 
         comments in their native language (we will translate) so that we can 
         continue the connection between different languages. Thank you!</p>
-<article id="blogPosts">    
-    <p class="postExample">put text here</p>
-    <p class="postExample">put text here</p>
+</article>
+<article id="blogPosts">
+    <p class="postExample">Hallo! Ich heiße Anna, und ich komme aus Deutschland. Ich studiere Literatur, 
+        und ich finde Übersetzung sehr wichtig, weil viele Länder besser miteinander 
+        kommunizieren können, wenn sie die Sprache der andere Länder lesen können. 
+        Aber viele Leute kann nur ein oder zwei Sprachen sprechen/lesen, und deshalb 
+        sind Übersetzer sehr wichtig. Wir brauchen immer Leute, die andere Sprachen 
+        können, damit wir alle miteinander sprechen können. Ich bin so froh, dass 
+        Translating Burlington Übersetzungen macht, und wir können für spezifische 
+        Texten erbitten. Sie sind sehr gut, die Übersetzungen sind realistisch, 
+        genau und verständlich, und ich werde sie anderen Leuten empfehlen. Vielen 
+        Dank für diese Webseite, sie ist sehr schön.</p>
+    <p>Hi! My name is Anna, and I’m from Germany. I study literature, and I think 
+        translation is very important. Countries can communicate with each other better 
+        when they can understand the languages of other countries. But a lot of people 
+        can only speak/read one or two languages, and therefore translators are very 
+        important. We always need more people that can speak other languages, so that 
+        we can all speak with each other. I’m very happy that Translation Burlington 
+        makes translations, and we can request specific texts to be translated. They’re 
+        really good, the translations are very realistic, exact, and understandable, 
+        and I will definitely recommend them to other people. Thank you so much for 
+        making this website, it’s amazing!</p>
     <!-- add more for more posts -->
 </article>
 <?php
