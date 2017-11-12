9060f59ba3c361c3a433ffff760833f45b3d3e35
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-12 16:14:16 -0500

    Update learn-more.php

--- learn-more.php
+++ learn-more.php
@@ -4,7 +4,29 @@ include ('top.php');
 <!--start of Origins article-->
 <article id="origin">
   <h1>The Origins of Language</h1>
-  <p>put content here</p>
+  <p>Language is perhaps the most important development in our history as humans. 
+      Though it is extremely difficult to know for sure how language began, and 
+      what the exact origins of language are, since we have no record of when/where/how 
+      it began, we do know that language has evolved a lot. A lot of study of language 
+      and linguistics begins with the beginning of writing systems, because they 
+      were the first to be able to document the spoken language. However, before 
+      that, much of the history of language was oral, which is usually inaccurate, 
+      and could not be documented until the invention of writing systems. Despite 
+      these difficulties, we have been able to study hundreds, even thousands, of 
+      years of the history of language. Language is continuously evolving, and the 
+      languages we speak today are often different than their original languages. 
+      There is so much to learn about language, and we are always discovering new 
+      things about language.</p>
+  <p>Most people don’t stop to think about how amazing it is that we can communicate 
+      what we think and feel and see using strings of characters or sounds, that 
+      to someone with no knowledge of a language, would seem like complete nonsense. 
+      However, we are all able to communicate in our own languages, most people 
+      having learned their native language without consciously trying, a feat that 
+      to this day baffles linguists and neuroscientists. We may never fully understand 
+      how we are able to speak and read and listen and communicate with others, 
+      but we can, and that is something really amazing. There will always be many 
+      unknowns about language and its origins, but what matters is that we continue 
+      to try to discover everything we can about spoken and written language.</p>
   <figure class="img-right"> <!---add more classes as needed-->
     <img class="" alt="" src="">
     <figcaption>put caption here</figcaption>
@@ -12,8 +34,34 @@ include ('top.php');
 </article>
 <!--start of Importance article, we may not need 2 articles, depends on CSS we want-->
 <article id="importance"> 
-  <h1>Why is Language Important?</h1>
-  <p>put content here</p>
+  <h1>Why is Translation Important?</h1>
+  <p>Learning a second language later in life is another phenomenon that amazes 
+      linguists and neuroscientists. When we learn our native language, we aren’t 
+      actively trying to learn; we just do. However, when we learn another language 
+      later in life, we learn it in a different way. We learn how to speak another 
+      language by learning its rules, and by learning to translate something from 
+      our own language into another language. Learning a language uses special 
+      parts of the brain, and studying how we learn a second language is extremely 
+      difficult but interesting. When we learn a second language, every time we 
+      speak or hear or read the second language, we translate into our own language.</p>
+  <p>This translation is extremely difficult and interesting, because each person 
+       has their own slightly different way of speaking a language. Translation 
+       is difficult to do because of this slight difference in meaning that each 
+       person understands, because of idiomatic and inflectional meanings of words 
+       in different languages. This is why a machine can never accurately translate 
+       like a human can - a machine doesn’t have the feelings and emotions that 
+       go into understanding and speaking a language. Only a human can accurately 
+       and meaningfully translate between languages, and that is only when one has 
+       a deep knowledge of these languages.</p>
+  <p>Translation allows people from two different cultures to communicate and 
+      share ideas and beliefs, as well as stories and feelings. There is nothing 
+      more amazing than being able to communicate with someone in their language, 
+      a feeling that many people take for granted in this world. Learning another 
+      language is one of the most important things you can do to enrich your life 
+      - open yourself up to the life and culture of another country by learning 
+      how to speak, listen and read another language. Allow translation to act as 
+      a learning mode, as well as an appreciation for other languages. Learn and 
+      never stop learning what it means to understand another language.</p>
   <figure class="img-right"> <!---add more classes as needed-->
     <img class="" alt="" src="">
     <figcaption>put caption here</figcaption>
