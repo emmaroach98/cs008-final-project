851aaf99fea969a3ce21955f4ac7b599ed84d0c4
mbostwick23 <33204834+mbostwick23@users.noreply.github.com>
2017-11-13 18:23:09 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,8 @@
 <!-- ######################     Start of header   ############################## -->
 <header class="headerTitle">
+    <figure class="logo">
+        <img alt="" src="finalproject/images/placeholder.jpg">
+    </figure>
     <h1>Translating Burlington</h1>
 </header>
 <!-- ######################     End of header   ############################## -->
