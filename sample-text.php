af285772285d72b6a2e51bc41b2b2960749dd5cd
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-11-13 19:09:49 -0500

    Update sample-text.php

--- sample-text.php
+++ sample-text.php
@@ -1,84 +1,86 @@
 <?php
- include ('top.php');
- ?>
- <article id="text1"> 
-   <h1>Sneewittchen - The Brothers Grimm, 1854</h1>
-   <p>Es war einmal mitten im Winter, und die Schneeflocken fielen wie Federn vom 
-    Himmel herab, da saß eine Königin an einem Fenster, das einen Rahmen von schwarzem 
-    Ebenholz hatte, und nähte. Und wie sie so nähte und nach dem Schnee aufblickte, 
-    stach sie sich mit der Nadel in den Finger, und es fielen drei Tropfen Blut in den 
-    Schnee. Und weil das Rote im weißen Schnee so schön aussah, dachte sie bei sich: 
-    "Hätt ich ein Kind so weiß wie Schnee, so rot wie Blut und so schwarz wie das Holz 
-    an dem Rahmen." Bald darauf bekam sie ein Töchterlein, das war so weiß wie Schnee, 
-    so rot wie Blut und so schwarzhaarig wie Ebenholz, und ward darum das Sneewittchen 
-    (Schneeweißchen) genannt. Und wie das Kind geboren war, starb die Königin.</p>
-   <p>Über ein Jahr nahm sich der König eine andere Gemahlin. Es war eine schöne Frau, 
-    aber sie war stolz und übermütig und konnte nicht leiden, daß sie an Schönheit von 
-    jemand sollte übertroffen werden. Sie hatte einen wunderbaren Spiegel, wenn sie vor 
-    den trat und sich darin beschaute, sprach sie: </br>"Spieglein, Spieglein an der Wand,</br>
-    wer ist die schönste im ganzen Land?
-   <p>So antwortete der Spiegel: "Frau Königin, Ihr seid die schönste im Land."</p>
-   <p>Da war sie zufrieden, denn sie wußte, daß der Spiegel die Wahrheit sagte.</p>
-   <!--add paragraphs as needed depending on content-->
- </article>
- <article id="text2"> 
-   <h1>Snow White - The Brothers Grimm 1854, translated by Emma Roach</h1>
-   <p>It was the middle of winter, and the snowflakes were falling like feathers from the 
-    sky. The queen was sitting at the window with a black ebony frame, and sewing. And 
-    while she was sewing, and looking at the snow, she pricked her finger with the needle, 
-    and 3 drops of blood fell onto the snow. And because the red looked so beautiful on the 
-    white snow, she thought, “If only I had a kid with skin as white as snow, lips as red as 
-    blood, and hair as black as the window frame’s ebony.” Soon after, she had a daughter 
-    that was exactly this: skin as white as snow, lips as red as blood, and hair as black as 
-    ebony, who was named Snow White. And as soon as the child was born, the queen died.</p>
-   <p>After a year, the king remarried. His new wife was a pretty woman, but she was proud 
-    and carefree and couldn’t stand that her beauty could be surpassed by others. She had a 
-    magical mirror, which she looked in and said,</br> “Mirror, mirror on the wall,</br> Who 
-    is the fairest of them all?”</p>
-   <p>The mirror would answer, “My Queen, you are the fairest of them all.”</p>
-   <p>Then she was satisfied, because she knew that the mirror only spoke the truth.</p>
-
-   <!--add paragraphs as needed depending on content-->
- </article>
- <article id="text3"> 
-   <h1>Hänsel und Gretel - The Brothers Grimm, 1812</h1>
-   <p>Vor einem großen Wald wohnte ein armer Holzhacker mit seiner Frau und seinen 
-    zwei Kindern; das Bübchen hieß Hänsel und das Mädchen Gretel. Er hatte wenig zu 
-    beißen und zu brechen, und einmal, als große Teuerung ins Land kam, konnte er 
-    auch das täglich Brot nicht mehr schaffen. Wie er sich nun abends im Bette Gedanken 
-    machte und sich vor Sorgen herumwälzte, seufzte er und sprach zu seiner Frau: 
-    "Was soll aus uns werden? Wie können wir unsere armen Kinder ernähren, da wir 
-    für uns selbst nichts mehr haben?" "Weißt du was, Mann", antwortete die Frau, 
-    "wir wollen morgen in aller Frühe die Kinder hinaus in den Wald führen, wo er 
-    am dicksten ist: da machen wir ihnen ein Feuer an und geben jedem noch ein Stückchen 
-    Brot, dann gehen wir an unsere Arbeit und lassen sie allein. Sie finden den Weg 
-    nicht wieder nach Haus, und wir sind sie los." "Nein, Frau", sagte der Mann, "das 
-    tue ich nicht; wie sollt' ich's übers Herz bringen, meine Kinder im Walde allein 
-    zu lassen die wilden Tiere würden bald kommen und sie zerreißen." "O du Narr", 
-    sagte sie, "dann müssen wir alle viere Hungers sterben, du kannst nur die Bretter 
-    für die Särge hobeln", und ließ ihm keine Ruhe, bis er einwilligte. "Aber die armen 
-    Kinder dauern mich doch", sagte der Mann.</p>
-   <!--add paragraphs as needed depending on content-->
- </article>
- <article id="text4"> 
-   <h1>Hansel and Gretel - The Brothers Grimm 1821, translated by Emma Roach</h1>
-   <p>A poor lumberjack lived outside a big forest with his wife and his two kids: the 
-       little boy was named Hansel, and the little girl was named Gretel. The lumberjack 
-       had very little to eat, and once, when the whole country was in a depression, he 
-       couldn’t even afford to buy their daily bread anymore. He was laying in bed one 
-       night, thinking and worrying. He sighed and asked his wife, “What shall become of 
-       us? How can we feed our poor kids, but not have anything left to feed ourselves?” 
-       “You know what, love?” answered his wife. “Tomorrow, early in the morning, let’s 
-       all go together into the thickest part of the woods. There we’ll make a fire, and 
-       give each of the kids a piece of bread, and then we’ll go about our work and leave 
-       them alone. They won’t find their way home, and then we’ll be rid of them.” “No, 
-       love,” said the man, “I can’t do that. How could I ever let myself leave my own 
-       kids alone in the woods to be eaten by wild animals?” “You fool,” she said, “then 
-       we’ll all die from hunger, and you can shape the boards for our coffins,” and she 
-       wouldn’t leave him alone until he agreed. “But the poor kids will surely outlast 
-       me,” said the man.</p>
-   <!--add paragraphs as needed depending on content-->
- </article>
- <?php
- include ('footer.php');
- ?>
+include ('top.php');
+?>
+<h1>German Texts translated to English</h1>
+<section id="snowWhite">
+    <article id="text1"> 
+        <h2>Sneewittchen - The Brothers Grimm, 1854</h2>
+        <p>Es war einmal mitten im Winter, und die Schneeflocken fielen wie Federn vom 
+        Himmel herab, da saß eine Königin an einem Fenster, das einen Rahmen von schwarzem 
+        Ebenholz hatte, und nähte. Und wie sie so nähte und nach dem Schnee aufblickte, 
+        stach sie sich mit der Nadel in den Finger, und es fielen drei Tropfen Blut in den 
+        Schnee. Und weil das Rote im weißen Schnee so schön aussah, dachte sie bei sich: 
+        "Hätt ich ein Kind so weiß wie Schnee, so rot wie Blut und so schwarz wie das Holz 
+        an dem Rahmen." Bald darauf bekam sie ein Töchterlein, das war so weiß wie Schnee, 
+        so rot wie Blut und so schwarzhaarig wie Ebenholz, und ward darum das Sneewittchen 
+        (Schneeweißchen) genannt. Und wie das Kind geboren war, starb die Königin.</p>
+        <p>Über ein Jahr nahm sich der König eine andere Gemahlin. Es war eine schöne Frau, 
+        aber sie war stolz und übermütig und konnte nicht leiden, daß sie an Schönheit von 
+        jemand sollte übertroffen werden. Sie hatte einen wunderbaren Spiegel, wenn sie vor 
+        den trat und sich darin beschaute, sprach sie: </br>"Spieglein, Spieglein an der Wand,</br>
+        wer ist die schönste im ganzen Land?
+        <p>So antwortete der Spiegel: "Frau Königin, Ihr seid die schönste im Land."</p>
+        <p>Da war sie zufrieden, denn sie wußte, daß der Spiegel die Wahrheit sagte.</p>
+    </article>
+    <article id="text2"> 
+        <h2>Snow White - The Brothers Grimm 1854, translated by Emma Roach</h2>
+        <p>It was the middle of winter, and the snowflakes were falling like feathers from the 
+        sky. The queen was sitting at the window with a black ebony frame, and sewing. And 
+        while she was sewing, and looking at the snow, she pricked her finger with the needle, 
+        and 3 drops of blood fell onto the snow. And because the red looked so beautiful on the 
+        white snow, she thought, “If only I had a kid with skin as white as snow, lips as red as 
+        blood, and hair as black as the window frame’s ebony.” Soon after, she had a daughter 
+        that was exactly this: skin as white as snow, lips as red as blood, and hair as black as 
+        ebony, who was named Snow White. And as soon as the child was born, the queen died.</p>
+        <p>After a year, the king remarried. His new wife was a pretty woman, but she was proud 
+        and carefree and couldn’t stand that her beauty could be surpassed by others. She had a 
+        magical mirror, which she looked in and said,</br> “Mirror, mirror on the wall,</br> Who 
+        is the fairest of them all?”</p>
+        <p>The mirror would answer, “My Queen, you are the fairest of them all.”</p>
+        <p>Then she was satisfied, because she knew that the mirror only spoke the truth.</p>
+    </article>
+</section>
+<section id="hanselGretel">
+    <article id="text3"> 
+        <h2>Hänsel und Gretel - The Brothers Grimm, 1812</h2>
+        <p>Vor einem großen Wald wohnte ein armer Holzhacker mit seiner Frau und seinen 
+        zwei Kindern; das Bübchen hieß Hänsel und das Mädchen Gretel. Er hatte wenig zu 
+        beißen und zu brechen, und einmal, als große Teuerung ins Land kam, konnte er 
+        auch das täglich Brot nicht mehr schaffen. Wie er sich nun abends im Bette Gedanken 
+        machte und sich vor Sorgen herumwälzte, seufzte er und sprach zu seiner Frau: 
+        "Was soll aus uns werden? Wie können wir unsere armen Kinder ernähren, da wir 
+        für uns selbst nichts mehr haben?" "Weißt du was, Mann", antwortete die Frau, 
+        "wir wollen morgen in aller Frühe die Kinder hinaus in den Wald führen, wo er 
+        am dicksten ist: da machen wir ihnen ein Feuer an und geben jedem noch ein Stückchen 
+        Brot, dann gehen wir an unsere Arbeit und lassen sie allein. Sie finden den Weg 
+        nicht wieder nach Haus, und wir sind sie los." "Nein, Frau", sagte der Mann, "das 
+        tue ich nicht; wie sollt' ich's übers Herz bringen, meine Kinder im Walde allein 
+        zu lassen die wilden Tiere würden bald kommen und sie zerreißen." "O du Narr", 
+        sagte sie, "dann müssen wir alle viere Hungers sterben, du kannst nur die Bretter 
+        für die Särge hobeln", und ließ ihm keine Ruhe, bis er einwilligte. "Aber die armen 
+        Kinder dauern mich doch", sagte der Mann.</p>
+    </article>
+    <article id="text4"> 
+        <h2>Hansel and Gretel - The Brothers Grimm 1821, translated by Emma Roach</h2>
+        <p>A poor lumberjack lived outside a big forest with his wife and his two kids: the 
+        little boy was named Hansel, and the little girl was named Gretel. The lumberjack 
+        had very little to eat, and once, when the whole country was in a depression, he 
+        couldn’t even afford to buy their daily bread anymore. He was laying in bed one 
+        night, thinking and worrying. He sighed and asked his wife, “What shall become of 
+        us? How can we feed our poor kids, but not have anything left to feed ourselves?” 
+        “You know what, love?” answered his wife. “Tomorrow, early in the morning, let’s 
+         all go together into the thickest part of the woods. There we’ll make a fire, and 
+        give each of the kids a piece of bread, and then we’ll go about our work and leave 
+        them alone. They won’t find their way home, and then we’ll be rid of them.” “No, 
+        love,” said the man, “I can’t do that. How could I ever let myself leave my own 
+        kids alone in the woods to be eaten by wild animals?” “You fool,” she said, “then 
+        we’ll all die from hunger, and you can shape the boards for our coffins,” and she 
+        wouldn’t leave him alone until he agreed. “But the poor kids will surely outlast 
+        me,” said the man.</p>
+    </article>
+</section>
+<?php
+include ('footer.php');
+?>
+</body>
+</html>
