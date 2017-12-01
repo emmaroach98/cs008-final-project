e6c191148e081dd806a585c95fb10fb4bd0d8563
emmaroach98 <33204836+emmaroach98@users.noreply.github.com>
2017-12-01 17:38:43 -0500

    Update top.php

--- top.php
+++ top.php
@@ -59,11 +59,11 @@ $path_parts = pathinfo($phpSelf);
         // notice this if statement only includes the functions if it is
         // form page. A common mistake is to make a form and call the page
         // join.php which means you need to change it below (or delete the if)
-        if ($path_parts['filename'] == "form") {
+        //if ($path_parts['filename'] == "form") {
             print PHP_EOL . '<!-- include form libraries -->' . PHP_EOL;
             include 'lib/validation-functions.php';
             include 'lib/mail-message.php';
-        }
+        //}
         
         print PHP_EOL . '<!-- finished including libraries -->' . PHP_EOL;
     ?>
